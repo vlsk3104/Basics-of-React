b1a3e5ef5a8f692fa2fc938503da21cd071d1ae7
kuro <70840151+vlsk3104@users.noreply.github.com>
2021-03-19 13:05:55 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <>
       <h1 style={{ color: "red" }}>こんにちは!!</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color="pink">元気です</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
